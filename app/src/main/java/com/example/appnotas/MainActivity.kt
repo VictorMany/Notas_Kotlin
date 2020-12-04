1f9a4ff04d448d6e4c9692348aee4e7cc74171f4
Victor Manuel Velazquez Fuentes <victor.velazfuentez@gmail.com>
2020-12-04 00:50:21 -0600

    CONTEXT NO DECLARED CORRECTLY

--- app/src/main/java/com/example/appnotas/MainActivity.kt
+++ app/src/main/java/com/example/appnotas/MainActivity.kt
@@ -20,33 +20,25 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-
-
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.layout_nota.view.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    lateinit var listaNotas: ArrayList<Nota>
-    val lista: ListView = findViewById(R.id.lista)
-    val lblTitulo: TextView = findViewById(R.id.lblTituloNota)
-    val lblFecha: TextView = findViewById(R.id.lblFecha)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
-        setContentView(R.layout.content_main)
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-
-        Inicializar()
-        Eventos()
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -56,40 +48,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
-
-    fun Inicializar() {
-        listaNotas = ArrayList()
-        listaNotas.add(Nota("Comprar despensa", "Naranjas, coca, aceite", "", "Viajes", "2/12/2020"))
-        listaNotas.add(Nota("Pagar la luz", "Naranjas, coca, aceite", "", "Viajes", "2/12/2020"))
-        listaNotas.add(Nota("Ir a misa", "Naranjas, coca, aceite", "", "Viajes", "2/12/2020"))
-
-        //TODO codigo adaptador
-        val adaptadorPersonas = AdaptadorNotas(this)
-        lista.adapter = adaptadorPersonas
-    }
-
-
-    fun Eventos() {
-        lista.setOnItemClickListener { adapterView, view, position, id ->
-            var intent = Intent(applicationContext, DetalleNota::class.java).apply {
-                putExtra("titulo", listaNotas[position]._titulo)
-                putExtra("fecha", listaNotas[position]._fecha)
-            }
-            startActivity(intent)
-        }
-
-    }
-
-    internal inner class AdaptadorNotas(context: AppCompatActivity)
-        : ArrayAdapter<Nota>(context,0,listaNotas){
-        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-            val inflater = convertView ?: LayoutInflater.from(context).inflate(R.layout.layout_nota, parent, false)
-            val currentNota = getItem(position)
-           // inflater.lblTitulo.text = currentNota!!._titulo
-           // inflater.lblFecha.text = currentNota!!._fecha
-            return inflater
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/example/appnotas/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/appnotas/ui/home/HomeFragment.kt
@@ -1,31 +1,58 @@
-package com.example.appnotas.ui.home
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import com.example.appnotas.Nota
 import com.example.appnotas.R
+import com.example.appnotas.ui.home.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.layout_nota.view.*
+import android.widget.ArrayAdapter as ArrayAdapter
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    lateinit var listaNotas: ArrayList<Nota>
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Inicializar()
+  
+    }
+
+    fun Inicializar() {
+        listaNotas = ArrayList()
+        listaNotas.add(Nota("Bill Gates", "Microsoft", "","",""))
+        listaNotas.add(Nota("Larry Page", "Google", "","",""))
+        listaNotas.add(Nota("Sergey Brin", "Google", "","",""))
+        
+        //TODO codigo adaptador
+        val adaptadorNotas = AdaptadorNotas(this)
+        lista.adapter = adaptadorNotas
+    }
+
+
+
+
+
+    internal inner class AdaptadorNotas(context: HomeFragment) //ANTES ERA APPCOMPACTACTIVITY
+        : ArrayAdapter<Nota>(context,0, listaNotas){
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val inflater = convertView ?: LayoutInflater.from(context).inflate(R.layout.layout_nota, parent, false)
+            val currentPersona = getItem(position)
+            inflater.lblTituloNota.text = currentPersona!!._titulo
+            inflater.lblFecha.text = currentPersona!!._fecha
+            return inflater
+        }
     }
 }
