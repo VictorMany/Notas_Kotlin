2fac0b5cb757994c48e663bc5cb1b9585851189f
Victor Manuel Velazquez Fuentes <victor.velazfuentez@gmail.com>
2020-12-03 19:15:24 -0600

    GUGA HABLADOR

--- app/src/main/java/com/example/appnotas/MainActivity.kt
+++ app/src/main/java/com/example/appnotas/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.example.appnotas
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -14,9 +21,15 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
+
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var listaNotas: ArrayList<Nota>
+    val lista: ListView = findViewById(R.id.lista)
+    val lblTitulo: TextView = findViewById(R.id.lblTituloNota)
+    val lblFecha: TextView = findViewById(R.id.lblFecha)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +37,16 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        setContentView(R.layout.content_main)
+
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        Inicializar()
+        Eventos()
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -38,6 +56,40 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    fun Inicializar() {
+        listaNotas = ArrayList()
+        listaNotas.add(Nota("Comprar despensa", "Naranjas, coca, aceite", "", "Viajes", "2/12/2020"))
+        listaNotas.add(Nota("Pagar la luz", "Naranjas, coca, aceite", "", "Viajes", "2/12/2020"))
+        listaNotas.add(Nota("Ir a misa", "Naranjas, coca, aceite", "", "Viajes", "2/12/2020"))
+
+        //TODO codigo adaptador
+        val adaptadorPersonas = AdaptadorNotas(this)
+        lista.adapter = adaptadorPersonas
+    }
+
+
+    fun Eventos() {
+        lista.setOnItemClickListener { adapterView, view, position, id ->
+            var intent = Intent(applicationContext, DetalleNota::class.java).apply {
+                putExtra("titulo", listaNotas[position]._titulo)
+                putExtra("fecha", listaNotas[position]._fecha)
+            }
+            startActivity(intent)
+        }
+
+    }
+
+    internal inner class AdaptadorNotas(context: AppCompatActivity)
+        : ArrayAdapter<Nota>(context,0,listaNotas){
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val inflater = convertView ?: LayoutInflater.from(context).inflate(R.layout.layout_nota, parent, false)
+            val currentNota = getItem(position)
+           // inflater.lblTitulo.text = currentNota!!._titulo
+           // inflater.lblFecha.text = currentNota!!._fecha
+            return inflater
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
---
+++ app/src/main/java/com/example/appnotas/Nota.kt
@@ -0,0 +1,10 @@
+package com.example.appnotas
+
+class Nota(titulo:String, descripcion:String, imagen:String, seccion:String, fecha:String) {
+    var _titulo = titulo
+    var _descripcion =  descripcion
+    var _imagen = imagen
+    var _seccion = seccion
+    var _fecha = fecha
+}
+
