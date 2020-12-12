ceb8b8c069d5f280d7f7abb484ae1d3a8d5a2af9
Felix <fcm68247@udelasalle.edu.mx>
2020-12-12 17:49:21 -0600

    Felix cambios de hoy  osea 12/12/2020

---
+++ app/src/main/java/com/example/appnotas/Modelo/DBOpenHelper.kt
@@ -0,0 +1,28 @@
+package com.example.appnotas.Modelo
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+val nombreDB = "NotasBD.db"
+val versionDB = 1
+
+
+class DbOpenHelper(context: Context) :
+    SQLiteOpenHelper(context, nombreDB,null, versionDB) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db!!.execSQL("""
+            create table tblNotas (
+                idNota integer primary key autoincrement,
+                TituloNota text not null,
+                DescripcionNota text not null,
+                imgNota text not null,
+                SeccionNota text not null,
+                FechaNota text not null
+            )
+        """.trimIndent())
+    }
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+    }
+}
